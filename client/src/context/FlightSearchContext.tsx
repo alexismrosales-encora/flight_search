da96d1bc3080e7f3c0f09d2bf1c1c5a489b104c0
alexismrosales <jorge.marquez@encora.com>
2025-05-17 19:38:50 -0600

    chore: context parameters added

---
+++ client/src/context/FlightSearchContext.tsx
@@ -0,0 +1,47 @@
+import { createContext, ReactNode, useState } from "react";
+import { FlightSearchContextType, SearchResultsStateType, DetailsInfoType } from "../types/FlightSearchContextTypes";
+import { FlightSearchResponse } from "../types/FlightSearchResponseTypes";
+
+export const FlightSearchContext = createContext<FlightSearchContextType | null>(null)
+
+export const FlighSearchProvider = ({ children }: { children: ReactNode }) => {
+  const [searchResults, setSearchResults] = useState<FlightSearchResponse[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
+
+  const [flight, setFlight] = useState<FlightSearchResponse | null>(null)
+
+  const [loadingFlight, setLoadingFlight] = useState<boolean>(false)
+  const [errorFlight, setErrorFlight] = useState<string | null>(null)
+  const [cityMap, setCityMap] = useState(new Map())
+
+  const searchResultsState: SearchResultsStateType = {
+    searchResults: searchResults,
+    setSearchResults: setSearchResults,
+    loading: loading,
+    setLoading: setLoading,
+    error: error,
+    setError: setError
+  }
+
+  const detailsInfo: DetailsInfoType = {
+    flight: flight,
+    setFlight: setFlight,
+    loadingFlight: loadingFlight,
+    setLoadingFlight: setLoadingFlight,
+    errorFlight: errorFlight,
+    setErrorFlight: setErrorFlight,
+    cityMap: cityMap,
+    setCityMap: setCityMap
+  }
+
+  const flightSearchContextType: FlightSearchContextType = {
+    searchResultsState: searchResultsState,
+    detailsInfo: detailsInfo
+  }
+
+  return <FlightSearchContext.Provider value={flightSearchContextType}>
+    {children}
+  </FlightSearchContext.Provider >
+}
+
